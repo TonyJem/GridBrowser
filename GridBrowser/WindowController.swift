04dcccc313c0d68300e01ad4f9a02fe9f589108f
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-25 09:56:29 +0300

    Create WindowController file

---
+++ GridBrowser/WindowController.swift
@@ -0,0 +1,18 @@
+//
+//  WindowController.swift
+//  GridBrowser
+//
+//  Created by Anton on 2020-09-25.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
