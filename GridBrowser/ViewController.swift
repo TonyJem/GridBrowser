940f9138eb0487e410aae2a0b8ba6145def6a9c4
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-25 11:07:56 +0300

    Add "WebKit" module and it's protocol

--- GridBrowser/ViewController.swift
+++ GridBrowser/ViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
