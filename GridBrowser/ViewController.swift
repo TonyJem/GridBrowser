807b21f5b091b356028af588bb612ad0646b7097
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-25 12:02:53 +0300

    Create StackView with initial Row and Column in it

--- GridBrowser/ViewController.swift
+++ GridBrowser/ViewController.swift
@@ -16,20 +16,38 @@ class ViewController: NSViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 1: Create the stack view and add it to our view
+        rows = NSStackView()
+        rows.orientation = .vertical
+        rows.distribution = .fillEqually
+        rows.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(rows)
+        
+        // 2: Create Auto Layout constrains that pins the stack view to the edges of its container
+        rows.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        rows.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        rows.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        rows.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        // 3: Create an initial column that contains a single webview
+        let column = NSStackView(views: [makeWebView()])
+        column.distribution = .fillEqually
+        
+        // 4: Add this column to the 'rows' stack view
+        rows.addArrangedSubview(column)
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-//    MARK: - IBActions
+    
+    //    MARK: - IBActions
     @IBAction func urlEntered(_ sender: NSTextField){
     }
-
+    
     @IBAction func navigationClicked(_ sender: NSSegmentedControl){
     }
     
@@ -39,7 +57,7 @@ class ViewController: NSViewController, WKNavigationDelegate {
     @IBAction func adjustColumns(_ sender: NSSegmentedControl){
     }
     
-//    MARK: - Methods
+    //    MARK: - Methods
     func makeWebView() -> NSView {
         let webView = WKWebView()
         webView.navigationDelegate = self
